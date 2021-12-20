d1f042ca691987f5b170ebc321a851c645671b3f
anudeepkota5 <40718888+anudeepkota5@users.noreply.github.com>
2021-12-21 01:12:50 +0530

    Update TestBase.java
    Updated with Dataprovider

--- src/main/java/com/sample/test/demo/TestBase.java
+++ src/main/java/com/sample/test/demo/TestBase.java
@@ -2,11 +2,20 @@ package com.sample.test.demo;
 
 import static org.testng.Assert.fail;
 
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.List;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+
+import com.sample.test.demo.utils.ExcelUtils;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -25,11 +34,11 @@ public class TestBase {
     }
 
     private void navigateToSite() {
-        ChromeOptions options = new ChromeOptions();
+    	ChromeOptions options = new ChromeOptions();
+    	options.addArguments("--headless");
     	options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--headless");
-        driver = new ChromeDriver(options);
+    	options.addArguments("--disable-dev-shm-usage");
+    	driver = new ChromeDriver(options);
         driver.get(url);
     }
 
@@ -51,5 +60,23 @@ public class TestBase {
         }
        
     }
+    
+    @DataProvider(name = "data")
+    public Iterator<Object[]> getData(Method M) throws Exception{
+    	List<Hashtable<String, String>> dataList = ExcelUtils.readFile(System.getProperty("user.dir")+"/src/test/resources/testdata/testData.xlsx", "data", M.getName());
+    	List<Object[]> testArray = new ArrayList<Object[]>();
+    	Iterator<Hashtable<String, String>> dataIterator = dataList.iterator(); 
+    	while(dataIterator.hasNext()){
+    		Hashtable<String, String> testdataset = dataIterator.next();
+    		if(!(testdataset.get("RunTest").equalsIgnoreCase("TRUE"))){
+    			dataIterator.remove();
+    		}else{
+    			Object[] dataObj = {testdataset};
+    			testArray.add(dataObj);
+    		}
+    	}
+    	return testArray.iterator();
+    }
+
 
 }
