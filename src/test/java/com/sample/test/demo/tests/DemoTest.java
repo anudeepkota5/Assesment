1e5f4ba659d06ab5f818dee2763ccbca009f6ed8
anudeepkota5 <40718888+anudeepkota5@users.noreply.github.com>
2021-12-21 01:11:34 +0530

    Update DemoTest.java
    Updated the code with dataprovider

--- src/test/java/com/sample/test/demo/tests/DemoTest.java
+++ src/test/java/com/sample/test/demo/tests/DemoTest.java
@@ -3,6 +3,8 @@ package com.sample.test.demo.tests;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertFalse;
 
+import java.util.Hashtable;
+
 import org.testng.annotations.Test;
 
 import com.sample.test.demo.TestBase;
@@ -21,35 +23,36 @@ public class DemoTest extends TestBase {
 	BookingPage book= null;
 	
 	
-    @Test(description = "Book an Fligh", priority = 2)
-    public void bookFlight() {
+    @Test(dataProvider = "data", description = "Book an Flight", priority = 1)
+    public void bookFlight(Hashtable<String, String> data) {
         
         home = new HomePage(driver);
-        home.selectFrom("Portland").selectTo("Dublin").clickfindFlights();
+        home.selectFrom(data.get("From")).selectTo(data.get("To")).clickfindFlights();
         
         flights = new FlightsPage(driver);
         flights.chooseFlight("1", "Virgin America");
         
         book = new BookingPage(driver);
-        book.enterName("Anudeep").enterAddress("Bnarayanapura").enterCity("Bangalore").enterstate("KA").enterzipCode("560016").selectCardType("American Express")
-        .enterCreditCardNumber("9999999999999999").enterCreditCardMonth("12").enterCreditCardYear("2023")
-        .enterNameOnCard("Anudeep Kota").clickPurchaseFlight();
+        book.enterName(data.get("Name")).enterAddress(data.get("Address")).enterCity(data.get("City")).enterstate(data.get("State"))
+        .enterzipCode(data.get("Zipcode")).selectCardType(data.get("CardType")).enterCreditCardNumber(data.get("CreditCardNumber"))
+        .enterCreditCardMonth(data.get("CreditCardMonth")).enterCreditCardYear(data.get("CreditCardYear"))
+        .enterNameOnCard(data.get("NameOnCard")).clickPurchaseFlight();
         
         BookingConfirmation conf = new BookingConfirmation(driver);
         assertFalse(conf.getConfirmationID().isEmpty());
         System.out.println(conf.getConfirmationID());
     }
     
-    @Test(description = "Validate API with Valid city name", priority = 0)
-	public void wetherAPIValidData(){
-		RestResponse<Model> resp = Openweathermap.getWether("Los Angeles");
+    @Test(dataProvider = "data",description = "Validate API with Valid city name", priority = 2)
+	public void wetherAPIValidData(Hashtable<String, String> data){
+		RestResponse<Model> resp = Openweathermap.getWether(data.get("From"));
 		assertEquals(resp.getStatusCode(), 200);
-		assertEquals(resp.getBody().getName(), "Los Angeles");
+		assertEquals(resp.getBody().getName(), data.get("From"));
 	}
 	
-	@Test(description = "Validate API with InValid city name", priority = 1)
-	public void wetherAPIInvalidData(){
-		RestResponse<Model> resp = Openweathermap.getWether("ABC");
+	@Test(dataProvider = "data",description = "Validate API with InValid city name", priority = 3)
+	public void wetherAPIInvalidData(Hashtable<String, String> data){
+		RestResponse<Model> resp = Openweathermap.getWether(data.get("From"));
 		assertEquals(resp.getStatusCode(), 404);
 	}
     
